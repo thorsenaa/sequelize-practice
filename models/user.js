7248a115bb1c74c702a4cc377f8e79d6e69be20c
Geoff Bass <geoff.c.bass@gmail.com>
2017-05-07 13:23:44 -0400

    add solution code

--- models/user.js
+++ models/user.js
@@ -1,10 +1,35 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/sequelize_practice');
+const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+  first: Sequelize.STRING,
+  last: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 18
+    }
+  },
+  bio: Sequelize.TEXT
 }, {
-// ...AND HERE
+  getterMethods: {
+    fullName: function () {
+      return `${this.first} ${this.last}`;
+    }
+  },
+  instanceMethods: {
+    haveBirthday: function () {
+      // return this.update({
+        // age: this.age + 1
+      // });
+      this.age++;
+      return this.save();
+    }
+  }
 });
 
 module.exports = User;
