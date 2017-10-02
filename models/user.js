e276a48166da01a2e47009dedd2d4be8b357d6f9
Annelise Thorsen <annelise.thorsen@gmail.com>
2017-10-01 22:42:35 -0400

    draft

--- models/user.js
+++ models/user.js
@@ -2,9 +2,53 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+    first: Sequelize.STRING,
+    last: Sequelize.STRING,
+    age: {
+        type: Sequelize.INTEGER,
+        validate: {
+            min: 18
+        }
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    bio: Sequelize.TEXT
 }, {
-// ...AND HERE
+    getterMethods: {
+        fullName: function() {
+            return this.first + ' ' + this.last
+        }
+    }
 });
+
+User.prototype.haveBirthday = function() {
+    // User.update({
+    //     where: {age:42}
+    // }).then((oldAge) =>{
+    //     console.log(oldAge)
+    // })
+
+    // User.findOne({
+    //     age: '42'
+    // })
+    // .then(user => {
+    //     user.updateAttributes({
+    //         age: '43'
+    //     });
+    // });
+    const newData = {
+        age: 43
+    };
+
+    User.update(newData, { where: { age: 42 }, 
+        returning: true,
+        plain: true
+    })
+    .then((updatedAge) => {
+        console.log(updatedAge)
+    }).save(updatedAge)
+}
 
 module.exports = User;
